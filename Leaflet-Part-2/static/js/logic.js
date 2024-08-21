c0997bf03cdbeec2692979adde478ce9c628412b
gunelq <gunelka91@gmail.com>
2024-08-21 18:06:16 -0500

    HW15

---
+++ Leaflet-Part-2/static/js/logic.js
@@ -0,0 +1,172 @@
+//Challenge:
+// Can I render a basic base map? - Set up Leaflet correctly
+// Can we fetch the data that we need to plot?
+
+// Helper function
+// Establish magnitude size
+// I want to make the differences in earthquake magnitudes more pronounced, I choose the larger scaling factor
+// helper function
+function markerSize(mag) {
+  let radius = 1;
+  if (mag > 0) {
+    radius = mag ** 7;
+  }
+
+  return radius
+}
+  
+  // Establish colors for depth
+  function mapColor(depth) {
+    switch (true) {
+      case depth > 90:
+        return "#EA2C2C";
+      case depth > 70:
+        return "#EA822C";
+      case depth > 50:
+        return "#EE9C00";
+      case depth > 30:
+        return "#EECC00";
+      case depth > 10:
+        return "#D4EE00";
+      default:
+        return "#98EE00";
+    }
+  }
+  
+  
+  function createMap(data, geo_data) {
+    // STEP 1: Init the Base Layers
+  
+    // Define variables for our tile layers.
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+  
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+  
+    // Step 2: Create the Overlay layers 
+    
+    let markers = L.markerClusterGroup();
+    let heatArray = [];
+    let circleArray = [];
+  
+    for (let i = 0; i < data.length; i++) {
+      //row=feature
+      let row = data[i];
+      let location = row.geometry;
+  
+      // Create marker
+      if (location) {
+        // Extract coord
+        let point = [location.coordinates[1], location.coordinates[0]];
+  
+        // Make marker
+        let marker = L.marker(point);
+        let popup = ("<br>Location: " + row.properties.place + "<br>Magnitude: " + row.properties.mag + "<br>Depth: " + row.geometry.coordinates[2]);
+        marker.bindPopup(popup);
+        markers.addLayer(marker);
+  
+        // Add to heatmap
+        heatArray.push(point);
+  
+        // Accessing the depth from the coordinates array 
+         let depth = row.geometry.coordinates[2];
+        // Create circle
+        let circleMarker = L.circle(point, {
+          fillOpacity:5,
+          color:  mapColor(depth),
+          fillColor: mapColor(depth),
+          radius: markerSize(row.properties.mag),
+          weight: 0.5,
+          opacity: 1,
+        }).bindPopup(popup);
+  
+        circleArray.push(circleMarker);
+      }
+    }
+  
+    // Create layer
+    let heatLayer = L.heatLayer(heatArray, {
+      radius: 25,
+      blur: 20
+    });
+  
+    let circleLayer = L.layerGroup(circleArray);
+  
+    // Tectonic plate layer
+    let geo_layer = L.geoJSON(geo_data, {
+      style: {
+        "color": "Orange",
+        "weight": 5
+      }
+    });
+  
+    // Step 3: BUILD the Layer Controls
+  
+    // Only one base layer can be shown at a time.
+    let baseLayers = {
+      Street: street,
+      Topography: topo
+    };
+  
+    let overlayLayers = {
+      Markers: markers,
+      Heatmap: heatLayer,
+      Circles: circleLayer,
+      "Tectonic Plates": geo_layer
+    };
+  
+    // Step 4: INIT the Map
+    let myMap = L.map("map", {
+      center: [40.7, -94.5],
+      zoom: 3,
+      layers: [street, markers,geo_layer]
+    });
+   
+  
+    // Add the Layer Control filter + legends as needed
+    L.control.layers(baseLayers, overlayLayers).addTo(myMap);
+  
+    let legend = L.control({ position: "bottomright" });
+  
+    legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend");
+      
+      // Add the header
+      let legendHeader = "<h4 class='legend-header'>Legend</h4>";
+      div.innerHTML = legendHeader;
+  
+      // Depth values for the legend
+      let depth = [-10, 10, 30, 50, 70, 90];
+  
+      // Loop through the depth values to create legend items
+      for (let i = 0; i < depth.length; i++) {
+        div.innerHTML +=
+          '<i style="background-color:' + mapColor(depth[i] + 1) + '">&emsp;&emsp;</i> ' +
+          depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+      }
+      
+      return div;
+    };
+  
+    legend.addTo(myMap);
+  }
+  
+  function doWork() {
+    // Assemble the API query URLs.
+    let EarthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    let tectonicplatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+  
+    d3.json(EarthquakeURL).then(function (data) {
+      d3.json(tectonicplatesURL).then(function (geo_data) {
+        let data_rows = data.features;
+  
+        // Create the map with both datasets
+        createMap(data_rows, geo_data);
+      });
+    });
+  }
+  
+  doWork();
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # leaflet-challenge
+Introduction
+In this challenge, the following information was provided as background: The United States Geological Survey (USGS) was responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists developed new methods and tools to supply timely, relevant, and useful information about the Earth and its processes. The USGS was interested in building a new set of tools that would allow them to visualize their earthquake data. They collected a massive amount of data from all over the world each day, but they lacked a meaningful way of displaying it. In this challenge, you were tasked with developing a way to visualize USGS data that would allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
+
+Instructions
+The instructions for this challenge were broken into two parts:
+
+Part 1: Created the Earthquake Visualization
+Part 2: Gathered and Plotted More Data
+Part 1: Created the Earthquake Visualization
+The first task was to visualize an earthquake dataset. The steps completed were:
+
+1. Got the dataset:
+The USGS provided earthquake data in a number of different formats, updated every 5 minutes. You visited the USGS GeoJSON Feed page and chose a dataset to visualize. The following image was an example screenshot of what appeared when visiting this link:
+
+
+
+
+
+When you clicked a dataset (such as "All Earthquakes from the Past 7 Days"), you were given a JSON representation of that data. You used the URL of this JSON to pull in the data for the visualization. The following image was a sampling of earthquake data in JSON format:
+
+
+
+2. Imported and visualized the data:
+Using Leaflet, you created a map that plotted all the earthquakes from the dataset based on their longitude and latitude.
+Data markers reflected the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes appeared larger, and earthquakes with greater depth appeared darker in color.
+Hint: The depth of the earth could be found as the third coordinate for each earthquake.
+You included popups that provided additional information about the earthquake when its associated marker was clicked.
+You created a legend that provided context for your map data.
+The visualization looked something like the preceding map.
+
+
+
+Part 2: Gathered and Plot More Data
+In this part, a second dataset was plotted on the map to illustrate the relationship between tectonic plates and seismic activity. The dataset was pulled in and visualized alongside the original data. Data on tectonic plates could be found at GitHub - Tectonic Plates. The following image was an example screenshot of the final product:
+
+
+
+Tasks performed:
+Plotted the tectonic plates dataset on the map in addition to the earthquakes.
+Added other base maps to choose from.
+Put each dataset into separate overlays that could be turned on and off independently.
+Added layer controls to the map.
+Folder and Files
+Leaflet-Part-1: Contained a static folder inside which had a css folder for styles and a js folder with a logic.js file (JavaScript code). This folder also contained the Mapbox API key for part 1 of this challenge.
+Leaflet-Part-2: Similar to Part 1, this folder contained a static folder with css and js folders, including the logic.js file and the Mapbox API key for part 2 of this challenge.
+Output: Contained images and videos about the map visualizations from part 1 and part 2.
+Free Static Page
+You can view the project on GitHub Pages at the following link:
+
+Leaflet-Part-1: https://isbelis.github.io/leaflet-challenge/Leaflet-Part-1/index.html
+Leaflet-Part-2: https://isbelis.github.io/leaflet-challenge/Leaflet-Part-2/index.html
+References
+Dataset created by the United States Geological Survey
+© 2024 edX Boot Camps LLC
